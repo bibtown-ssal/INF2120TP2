8fa8d9133885cc623d0921d4d736569a457d200b
Charles Therien <therien.charles@courrier.uqam.ca>
2018-06-08 23:20:40 -0400

    removed useless comments

--- src/Pair.java
+++ src/Pair.java
@@ -4,8 +4,7 @@ public class Pair<T, U> {
     public U deuxieme;
 
     public Pair( T premiere, U deuxieme ) {
-        //a instanceof U. illegal.
-        //deuxieme instanceof Fraction. legal.
+
         this.premiere = premiere;
         this.deuxieme = deuxieme;
     }
